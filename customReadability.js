6763dee0ef9d878c2710691702e3ba1ed49e0e48
Mihai Rotaru <mihai.rotaru@gmx.com>
2015-08-22 10:30:42 +0100

    upd readability

--- customReadability.js
+++ customReadability.js
@@ -2,8 +2,10 @@
   var article = document.querySelector(".hentry");
   article.style.maxWidth = "920px"; 
   article.style.fontFamily = "Delicious"; 
-  var pres = document.querySelectorAll("pre");
+  article.style.lineHeight = "1.2"; 
+  var pres = document.querySelectorAll("pre, code");
   for (var i=0; i < pres.length; ++i) {
-    pres[i].style.fontFamily = '"Envy Code", "Lucida Console", "Courier New", Courier, monospace"';
+    pres[i].style.fontFamily = '"Envy Code R", "Lucida Console", "Courier New", Courier, monospace"';
+    pres[i].style.lineHeight = "1.2"; 
   }
 })()
